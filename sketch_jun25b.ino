36891ab793d548dff1726fab5974347e04041d42
grapesss <58192955+grapesss@users.noreply.github.com>
2020-06-29 19:28:54 -0400

    Changed the display to I2C OLED

--- sketch_jun25b.ino
+++ sketch_jun25b.ino
@@ -1,134 +1,51 @@
 // Run sudo chmod a+rw /dev/ttyACM0 before running this script if you're using Linux
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-int pinA = 2;
-int pinB = 3;
-int pinC = 4;
-int pinD = 5;
-int pinE = 6;
-int pinF = 7;
-int pinG = 8;
-int D1 = 9;
-int D2 = 10;
-int D3 = 11;
-int D4 = 12;
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-void digit(int digit){
-  digitalWrite(D1, LOW);
-  digitalWrite(D2, LOW);
-  digitalWrite(D3, LOW);
-  digitalWrite(D4, LOW);
+int count = 0;
+const int button = 8;
+int pressed;
+int buttonState = 0;
 
-  digitalWrite(digit, HIGH);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup(){
+  pinMode(button, INPUT);
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println("SSD1306 allocation failed");
+    for(;;);
+  }
+  delay(2000);
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  display.setCursor(0, 5);
+  display.println("Button Counter");
+  display.display();
 }
 
-void printNumber(int number, int digit){
-  digit(digit);
-  if (number == 0){
-    digitalWrite(pinA, LOW);   
-    digitalWrite(pinB, LOW);   
-    digitalWrite(pinC, LOW);   
-    digitalWrite(pinD, LOW);   
-    digitalWrite(pinE, LOW);   
-    digitalWrite(pinF, LOW);   
-    digitalWrite(pinG, HIGH);
-  }
-  if (number == 1){
-    digitalWrite(pinA, HIGH);   
-    digitalWrite(pinB, LOW);   
-    digitalWrite(pinC, LOW);   
-    digitalWrite(pinD, HIGH);   
-    digitalWrite(pinE, HIGH);   
-    digitalWrite(pinF, HIGH);   
-    digitalWrite(pinG, HIGH);
-  }
-  if (number == 2){
-    digitalWrite(pinA, LOW);   
-    digitalWrite(pinB, LOW);   
-    digitalWrite(pinC, HIGH);   
-    digitalWrite(pinD, LOW);   
-    digitalWrite(pinE, LOW);   
-    digitalWrite(pinF, HIGH);   
-    digitalWrite(pinG, LOW);
-  }
-  if (number == 3){
-    digitalWrite(pinA, LOW);   
-    digitalWrite(pinB, LOW);   
-    digitalWrite(pinC, LOW);   
-    digitalWrite(pinD, LOW);   
-    digitalWrite(pinE, HIGH);   
-    digitalWrite(pinF, HIGH);   
-    digitalWrite(pinG, LOW);
-  }
-  if (number == 4){
-    digitalWrite(pinA, HIGH);   
-    digitalWrite(pinB, LOW);   
-    digitalWrite(pinC, LOW);   
-    digitalWrite(pinD, HIGH);   
-    digitalWrite(pinE, HIGH);   
-    digitalWrite(pinF, LOW);   
-    digitalWrite(pinG, LOW);
-  }
-  if (number == 5){
-    digitalWrite(pinA, LOW);   
-    digitalWrite(pinB, HIGH);   
-    digitalWrite(pinC, LOW);   
-    digitalWrite(pinD, LOW);   
-    digitalWrite(pinE, HIGH);   
-    digitalWrite(pinF, LOW);   
-    digitalWrite(pinG, LOW);
-  }
-  if (number == 6){
-    digitalWrite(pinA, LOW);   
-    digitalWrite(pinB, HIGH);   
-    digitalWrite(pinC, LOW);   
-    digitalWrite(pinD, LOW);   
-    digitalWrite(pinE, LOW);   
-    digitalWrite(pinF, LOW);   
-    digitalWrite(pinG, LOW);
-  }
-  if (number == 7){
-    digitalWrite(pinA, LOW);   
-    digitalWrite(pinB, LOW);   
-    digitalWrite(pinC, LOW);   
-    digitalWrite(pinD, HIGH);   
-    digitalWrite(pinE, HIGH);   
-    digitalWrite(pinF, HIGH);   
-    digitalWrite(pinG, HIGH);
-  }
-  if (number == 8){
-    digitalWrite(pinA, LOW);   
-    digitalWrite(pinB, LOW);   
-    digitalWrite(pinC, LOW);   
-    digitalWrite(pinD, LOW);   
-    digitalWrite(pinE, LOW);   
-    digitalWrite(pinF, LOW);   
-    digitalWrite(pinG, LOW);    
-  }
-  if (number == 9){
-    digitalWrite(pinA, LOW);   
-    digitalWrite(pinB, LOW);   
-    digitalWrite(pinC, LOW);   
-    digitalWrite(pinD, HIGH);   
-    digitalWrite(pinE, HIGH);   
-    digitalWrite(pinF, LOW);   
-    digitalWrite(pinG, LOW);
-  }
-  }
-
+void loop(){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  buttonState = digitalRead(button);
   
-void setup() {
-  pinMode(pinA, OUTPUT);     
-  pinMode(pinB, OUTPUT);     
-  pinMode(pinC, OUTPUT);     
-  pinMode(pinD, OUTPUT);     
-  pinMode(pinE, OUTPUT);     
-  pinMode(pinF, OUTPUT);     
-  pinMode(pinG, OUTPUT);   
-  pinMode(D1, OUTPUT);  
-  pinMode(D2, OUTPUT);  
-  pinMode(D3, OUTPUT);  
-  pinMode(D4, OUTPUT);
-}
+  if (buttonState != 0){
+    display.setCursor(0, 30);
+    display.println("LED on");
+  }
 
-void loop() {
+  else{
+    display.setCursor(0, 30);
+    display.println("LED off");
+  }
+  display.display();
 }
